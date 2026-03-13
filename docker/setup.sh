e39b4262d1f42a5d1a3aa81d4a822e6f5a39e1c2
liu <lzai80800@gmail.com>
2026-03-13 17:18:17 +0000

    fix script error

--- docker/setup.sh
+++ docker/setup.sh
@@ -8,11 +8,11 @@ if [ -n "$1" ]; then
   export SET_PASS="$1"
 fi
 if [ ! -f /data/secret_key.txt ]; then
-    uv --frozen run python3 -c "from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())" > /data/secret_key.txt
+    uv run --frozen python3 -c "from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())" > /data/secret_key.txt
     echo "New secret generated."
 fi
-uv --frozen run manage.py migrate
-uv --frozen run manage.py fake_data
+uv run --frozen manage.py migrate
+uv run --frozen manage.py fake_data
 
 chown -R www-data:www-data /data
 chmod -R 775 /data
