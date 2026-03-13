eefc570f4de6cdd45cbe868563af0b9302192628
liu <lzai80800@gmail.com>
2026-03-13 17:15:13 +0000

    only generate secret if no secret

--- docker/setup.sh
+++ docker/setup.sh
@@ -7,7 +7,10 @@ export DJANGO_SETTINGS_MODULE='backend.prod_settings'
 if [ -n "$1" ]; then
   export SET_PASS="$1"
 fi
-uv --frozen run python3 -c "from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())" > /data/secret_key.txt
+if [ ! -f /data/secret_key.txt ]; then
+    uv --frozen run python3 -c "from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())" > /data/secret_key.txt
+    echo "New secret generated."
+fi
 uv --frozen run manage.py migrate
 uv --frozen run manage.py fake_data
 
