261e7ed95518701d8472c089968bfec21f9d5c4d
liu <lzai80800@gmail.com>
2026-03-13 17:34:00 +0000

    this should prevent reinstalling at setup

--- docker/setup.sh
+++ docker/setup.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-cd /app/backend
+cd /app
 export DJANGO_SETTINGS_MODULE='backend.prod_settings'
 if [ -n "$1" ]; then
   export SET_PASS="$1"
@@ -11,8 +11,8 @@ if [ ! -f /data/secret_key.txt ]; then
     uv run --frozen python3 -c "from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())" > /data/secret_key.txt
     echo "New secret generated."
 fi
-uv run --frozen manage.py migrate
-uv run --frozen manage.py fake_data
+uv run --frozen backend/manage.py migrate
+uv run --frozen backend/manage.py fake_data
 
 chown -R www-data:www-data /data
 chmod -R 775 /data
