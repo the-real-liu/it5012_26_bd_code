6a506cac54c21f5b1af9042d0dec7c7acf16d2ae
liu <lzai80800@gmail.com>
2026-03-13 17:09:20 +0000

    freeze when setup

--- docker/setup.sh
+++ docker/setup.sh
@@ -7,9 +7,9 @@ export DJANGO_SETTINGS_MODULE='backend.prod_settings'
 if [ -n "$1" ]; then
   export SET_PASS="$1"
 fi
-uv run python3 -c "from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())" > /data/secret_key.txt
-uv run manage.py migrate
-uv run manage.py fake_data
+uv --frozen run python3 -c "from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())" > /data/secret_key.txt
+uv --frozen run manage.py migrate
+uv --frozen run manage.py fake_data
 
 chown -R www-data:www-data /data
 chmod -R 775 /data
