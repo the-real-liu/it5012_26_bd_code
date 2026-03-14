54adf71dcc3d5c846584394e0293ebf35a4b18f9
liu <lzai80800@gmail.com>
2026-03-14 23:45:34 +0000

    change prod to disable session auth too

--- backend/backend/prod_settings.py
+++ backend/backend/prod_settings.py
@@ -110,7 +110,7 @@ AUTHENTICATION_BACKENDS = [
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
     ]
 }
 
